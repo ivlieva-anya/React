bf91145e8aafea573883650569485189ce33f745
ivlieva-anya <ivlieva.anya@mail.ru>
2019-01-23 18:44:08 +0300

    l3

---
+++ myApp/my-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const SearchPanel = () =>{
+    return(
+        <input
+            className="form-control seach-input"
+            type="text"
+            plaseholder="Поиск по записям"
+        />
+        
+    )
+}
+export default SearchPanel;
